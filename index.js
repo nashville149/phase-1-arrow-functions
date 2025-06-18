b4f9a7c956d3b4531907f80c5da9e04d6ccc1423
nashville149 <milliamkarambu76@gmail.com>
2025-06-18 20:05:21 +0300

    complete

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// Function expression called divide
+const divide = function() {
+  return 2000 / 100;
+};
+
+// Arrow function called square
+const square = (x) => x * x;
+
+// Arrow function called add
+const add = (a, b) => a + b;
